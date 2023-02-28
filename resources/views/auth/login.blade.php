06bd0faf78be21577d41437220b0a4676394a745
TuCodeOnGit <tuminhvo98@gmail.com>
2023-02-28 13:33:00 +0700

    build: login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,31 +13,17 @@
             <form class="form-horizontal auth-form" method="POST"
                 action="{{ route('login') }}">
                 @csrf
-                {{-- <div class="form-group">
-                    <input autofocus required="" name="login[username]" type="email"
-                        class="form-control" placeholder="Username" id="email">
-                </div> --}}
-
                 <div class="form-group">
-                    {{-- <x-input-label for="email" :value="__('Email')" /> --}}
                     <x-text-input id="email" class="form-control" type="email"
                         name="email" :value="old('email')" placeholder="Email" required
                         autofocus />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
-                {{-- 
-                <div class="form-group">
-                    <input required="" id="password" name="login[password]" type="password" name="Password"
-                        class="form-control" placeholder="Password" autocomplete="current-password">
-                </div> --}}
 
                 <div class="form-group">
-                    <!-- <x-input-label for="password" :value="__('Password')" /> -->
-
                     <x-text-input id="password" class="form-control" type="password"
                         name="password" placeholder="Password" required
                         autocomplete="current-password" />
-
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
                 <div class="form-terms">
@@ -61,52 +47,10 @@
                         <li><a class="icon-facebook" href="{{ route('socialLogin', 'facebook') }}"></a></li>
                         <li><a class="icon-google" href="{{ route('socialLogin', 'google') }}"></a></li>
                     </ul>
-                </div>
-            </form>
+                  </div>
+                  <a class="d-block mt-3" href="{{ route('register') }}">Create an account</a>
+                </form>
         </div>
     </div>
+    <a href="{{ route('home') }}" class="btn btn-primary back-btn"><i data-feather="arrow-left"></i>back</a>
 </x-guest-layout>
-                            
-{{-- 
-<x-guest-layout>
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form></x-guest-layout> --}}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,12 +12,6 @@
             <form class="form-horizontal auth-form" method="POST"
                 action="{{ route('register') }}">
                 @csrf
-                {{-- <div class="form-group">
-                    <input required="" name="login[username]" type="email"
-                        class="form-control" placeholder="Username"
-                        id="registerEmail">
-                </div> --}}
-
                 <div class="form-group">
                     <x-text-input id="name" class="form-control" type="text"
                         name="name" :value="old('name')" placeholder="Username" required
@@ -26,23 +20,10 @@
                 </div>
 
                 <div class="form-group">
-                    {{-- <x-input-label for="email" :value="__('Email')" /> --}}
                     <x-text-input id="email" class="form-control" type="email"
                         name="email" :value="old('email')" placeholder="Email" required/>
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
-
-                {{-- <div class="form-group">
-                    <input required="" name="login[password]" type="password"
-                        class="form-control" placeholder="Password"
-                        id="registerPassword">
-                </div>
-                <div class="form-group">
-                    <input required="" name="login[password]" type="password"
-                        class="form-control" placeholder="Confirm Password"
-                        id="ConfirmPassword">
-                </div> --}}
-
                 <div class="form-group">
                     <x-text-input id="password" class="form-control"
                         type="password" name="password" placeholder="Password" />
@@ -53,22 +34,6 @@
                         type="password" name="password_confirmation" placeholder="Confirm Password" />
                     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                 </div>
-                <!-- <div class="form-terms">
-                    <div class="custom-control custom-checkbox form-check mr-sm-2">
-                        <input class="form-check-input" type="checkbox" value=""
-                            id="customControlAutosizing1">
-                        <label class="custom-control-label form-check-label"
-                            for="customControlAutosizing1"><span>I agree all statements in
-                                <a href="" class="pull-right">Terms &amp;
-                                    Conditions</a></span></label>
-                    </div>
-                    <!-- <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
-                    Default checkbox
-                    </label>
-                </div> -->
-                <!-- </div> -->
                 <div class="form-button">
                     <button class="btn btn-primary" type="submit">Register</button>
                 </div>
@@ -78,62 +43,10 @@
                         <li><a class="icon-facebook" href="{{ route('socialLogin', 'facebook') }}"></a></li>
                         <li><a class="icon-google" href="{{ route('socialLogin', 'google') }}"></a></li>
                     </ul>
-                </div>
+                  </div>
+                  <a class="d-block mt-3" href="{{ route('login') }}">Login</a></span>
             </form>
         </div>
     </div>
+    <a href="{{ route('home') }}" class="btn btn-primary back-btn"><i data-feather="arrow-left"></i>back</a>
 </x-guest-layout>
-
-
-{{-- <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
