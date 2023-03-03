6e44f7ac42411a460b35bd963930928306ce47f8
TuCodeOnGit <tuminhvo98@gmail.com>
2023-03-03 10:07:17 +0700

    build: profile

---
+++ resources/views/fe/profile.blade.php
@@ -0,0 +1,124 @@
+@extends('fe.layout')
+
+@section('contents')
+<!-- breadcrumb start -->
+<div class="breadcrumb-main ">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="breadcrumb-contain">
+                    <div>
+                        <h2>profile</h2>
+                        <ul>
+                            <li><a href="index.html">home</a></li>
+                            <li><i class="fa fa-angle-double-right"></i></li>
+                            <li><a href="javascript:void(0)">profile</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- breadcrumb End -->
+
+<!-- personal deatail section start -->
+<section class="contact-page register-page section-big-py-space b-g-light">
+    <div class="custom-container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h3 class="mb-3">PERSONAL DETAIL</h3>
+                <form class="theme-form">
+                    <div class="row">
+                        <div class="col-md-6">
+                           <div class="form-group">
+                               <label >First Name</label>
+                               <input type="text" class="form-control"  placeholder="Enter Your name" required="">
+                           </div>
+                        </div>
+                        <div class="col-md-6">
+                           <div class="form-group">
+                               <label >Last Name</label>
+                               <input type="text" class="form-control"  placeholder="Last Name" required="">
+                           </div>
+                        </div>
+                        <div class="col-md-6">
+                           <div class="form-group">
+                               <label >Phone number</label>
+                               <input type="text" class="form-control" placeholder="Enter your number" required="">
+                           </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <div class="form-group">
+                                    <label >Email</label>
+                                    <input type="text" class="form-control" placeholder="Email" required="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div >
+                                <label >Write Your Message</label>
+                                <textarea class="form-control mb-0" placeholder="Write Your Message"  rows="6"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-6">
+                <h3 class="mb-3 spc-responsive">SHIPPING ADDRESS</h3>
+                <form class="theme-form">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label >flat / plot</label>
+                                <input type="text" class="form-control"  placeholder="company name" required="">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label >Address *</label>
+                                <input type="text" class="form-control"  placeholder="Address" required="">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                           <div class="form-group">
+                               <label >Zip Code *</label>
+                               <input type="text" class="form-control"  placeholder="zip-code" required="">
+                           </div>
+                        </div>
+                        <div class="col-md-6 select_input">
+                            <div class="form-group">
+                                <label >Country *</label>
+                                <select class="form-control" size="1">
+                                    <option value="India">India</option>
+                                    <option value="UAE">UAE</option>
+                                    <option value="U.K">U.K</option>
+                                    <option value="US">US</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                           <div class="form-group">
+                               <label >City *</label>
+                               <input type="text" class="form-control"  placeholder="City" required="">
+                           </div>
+                        </div>
+                        <div class="col-md-6">
+                           <div class="form-group">
+                               <label >Region/State *</label>
+                               <input type="text" class="form-control"  placeholder="Region/state" required="">
+                           </div>
+                        </div>
+                        <div class="col-md-12">
+                            <button class="btn btn-sm btn-normal mb-lg-5" type="submit">Save setting</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Section ends -->
+
+@endsection
