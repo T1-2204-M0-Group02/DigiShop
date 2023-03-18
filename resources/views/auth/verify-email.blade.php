d41f3fd73922d9685325d05cf4f6a0bd368c3935
TuCodeOnGit <tuminhvo98@gmail.com>
2023-03-18 15:44:58 +0700

    build: mail, pagination, verify

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -2,16 +2,15 @@
 
 @section('contents')
 
-
 <!-- section start -->
 <section class="p-0 b-g-light">
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
                 <div class="error-section">
-                    <div class="mb-4 text-sm text-gray-600">
+                    <p class="mb-4 h5 text-gray-600">
                         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-                    </div>
+                    </p>
 
                     @if (session('status') == 'verification-link-sent')
                         <div class="mb-4 font-medium text-sm text-green-600">
@@ -25,19 +24,12 @@
 
                             <div>
                                 <button type="submit" class="btn btn-normal btn-outline tooltip-top">Resend Verification Email</button>
-
-                                {{-- <x-primary-button>
-                                    {{ __('Resend Verification Email') }}
-                                </x-primary-button> --}}
                             </div>
                         </form>
 
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <button type="submit" class="btn btn-normal mt-4">Log Out</button>
-                            {{-- <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                {{ __('Log Out') }}
-                            </button> --}}
                         </form>
                     </div>
                 </div>
